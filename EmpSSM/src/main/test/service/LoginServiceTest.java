945531eec0b9451f650344bfeb1ca958dfcd918b
Gary Wong <13296540788@163.com>
2017-12-03 12:08:10 +0800

    spring test
    1.引入spring test的测试环境

---
+++ EmpSSM/src/main/test/service/LoginServiceTest.java
@@ -0,0 +1,29 @@
+package service;
+
+import dao.LoginMapper;
+import model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * create by Gary Wong
+ * 2017/12/3
+ * class describetion : none
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = {"classpath:applicationContext.xml"})
+public class LoginServiceTest {
+
+    @Resource
+    private LoginMapper loginMapper;
+
+    @Test
+    public void test(){
+        User user = loginMapper.loginByUsernameAndPassword("admin", "123456");
+        System.out.println(user);
+    }
+}
